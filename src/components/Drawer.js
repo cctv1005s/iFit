58c022f8952665874edfb2c70e6ff79b88ec6ace
gorgear <384113872@qq.com>
2017-04-20 09:32:24 +0800

    添加了ScollView的内容

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,5 +1,22 @@
 import React ,{Component}from 'react';
-import {View,Text,Image,Button} from 'react-native';
+import {View,Text,Image,Button,StyleSheet,TouchableOpacity } from 'react-native';
+
+
+var style = StyleSheet.create({
+    view:{
+        alignItems:'flex-start',
+        marginTop:20
+    },
+    img:{
+        height:20,
+        width:20
+    },
+    text:{
+        height:20,
+        width:20
+    }
+});
+
 
 export default class Drawer extends Component{
     render(){
@@ -7,12 +24,24 @@ export default class Drawer extends Component{
         return (
             <View>
                 <Image source={require('../assets/img/background.png')} style={{width:300,height:250}}/>
-                <Button title="text me" onPress={
+                
+                <TouchableOpacity 
+                 onPress={
                     ()=>{
-                        navigation.navigate('Collect');
+                         navigation.navigate('Note');
                     }
-                }/>
+                 }
+                 style={
+                     style.view
+                 }
+                >
+                    <Image source={require('../assets/img/note.png')} style={style.img}/>
+                    <Text style={style.text}>健身笔记</Text>
+                </TouchableOpacity>
+
             </View>
         );
     }
 }
+
+
--- src/components/Information/FitTime.js
+++ src/components/Information/FitTime.js
@@ -1,29 +1,81 @@
 import React,{Component} from 'react';
-import {View,Text,Image} from 'react-native';
+import {View,Text,Image,Button,ScrollView} from 'react-native';
 import { TabNavigator } from 'react-navigation';
-
+import Header from '../Header.js';
+import cheerio from 'cheerio-without-node-native';
 
 /**
  * 概览
  */
 export default class FitTime extends Component{
-  static navigationOptions = {
-    tabBar: {
-      label: 'FitTime',
-      icon: ({ tintColor }) => (
-        <Image
-          source={require('../../assets/img/fittime.png')}
-          style={{height:18,width:18}}
-        />
-      ),
-    },
+    constructor(p){
+        super(p);
+        this.state = {
+            list:[]
+        };
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch('http://cn.bing.com/images/search?q=image&FORM=HDRSC2')
+        .then(function(res){
+            return res.text();
+        })
+        .then(function(text){
+            var $ = cheerio.load(text);
+            var thumbs = $('.thumb');
+            for(var i = 0;i < thumbs.length;i++){
+                self.state.list.push({
+                    url:thumbs[i].attribs.href,
+                    name:'thumbs'
+                });
+            }
+            self.setState({});
+        })
+        .catch(function(e){
+            throw new Error(e);
+        });
+    }
+
+    static navigationOptions = {
+        tabBar: {
+        label: 'FitTime',
+        icon: ({ tintColor }) => (
+            <Image
+            source={require('../../assets/img/fittime.png')}
+            style={{height:18,width:18}}
+            />
+        ),
+        },
     };
+
     render(){
+        console.log(this.state);
         return (
             <View>
-                <Text>
-                    概览
-                </Text>
+                <Header 
+                    title = "FitTime"
+                    leftClick={()=>{
+                        global.navigation.navigation.navigate('DrawerOpen')
+                    }}
+                />
+                <View>
+                <ScrollView style={{height:600}}>
+                    {this.state.list.map(function(ele,i){
+                        return (
+                            <View key={i}>
+                            <Image 
+                             source={{uri: ele.url}}
+                             style={{width: 100, height: 100}} 
+                            />
+                            <Text>
+                                我是图片
+                            </Text>
+                            </View>
+                        );
+                    })}
+                </ScrollView>
+                </View>
             </View>
         );
     }
