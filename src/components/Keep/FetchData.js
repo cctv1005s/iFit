a40bb431b460ab827483d39a7d8c559397201553
huangjingping <nkuhjp@163.com>
2017-04-26 16:11:27 +0800

    添加了Keep和SinaWeibo模块,修改了之前fetchData方法

---
+++ src/components/Keep/FetchData.js
@@ -0,0 +1,62 @@
+  import React from 'react';
+  import cheerio from 'cheerio-without-node-native';
+
+  var FetchDatas = {};
+
+  var _datasource = [];
+
+  var getData = function(self,index,size){
+    if(index > size){
+      var realDatasource = [];
+          console.log("keep: " + _datasource.length / 4);
+          for (var i = 0, j = 0; i < _datasource.length / 4; i++ , j += 4) {
+            realDatasource.push([
+               _datasource[j],
+               _datasource[j + 1],
+               _datasource[j + 2],
+               _datasource[j + 3],
+            ]);
+          }
+          console.log(realDatasource);
+          self.setState({
+            dataSource: self.state.dataSource.cloneWithRows(realDatasource),
+            loaded: true,
+          });
+
+          //讲数据添加进入缓存
+          self.saveData();
+    }else{
+      var url = 'http://www.hiyd.com/bb/?page='+index;
+      fetch(url)
+      .then(res => { return res.blob(); })
+      .then(blob => {
+        var reader = new FileReader();
+        reader.readAsText(blob, 'UTF-8');
+        reader.onload = function (e) {
+          var $ = cheerio.load(reader.result);
+          var allText = $('.train-title a');
+          var allPhoto = $('.train-pic img');
+          for (var i = 0; i < allText.length; i++) {
+            _datasource.push({
+              url: allText[i].attribs.href,
+              title: allPhoto[i].attribs.alt,
+              photo: allPhoto[i].attribs.src
+            });
+          }    
+          
+            getData(self,index+1,size);
+           }
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+    }
+  }
+
+
+
+  FetchDatas.fetchData = function(self,index,size) {
+    getData(self,index,size);
+  }
+
+  export default FetchDatas;
---
+++ src/components/SinaWeibo/SinaWeiboStyles.js
@@ -0,0 +1,79 @@
+import {
+   StyleSheet,
+   Dimensions,
+   Image,
+} from 'react-native';
+
+var {width,height} = Dimensions.get('window');
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F0',
+  },
+  ImageTextContainer: {
+    //borderWidth: 3,
+    margin: 10,
+    backgroundColor: 'white',
+  },
+  bigContainer: {
+    margin:10,
+  },
+  smallContainer: {
+     marginBottom:5,
+     marginHorizontal: 5,
+     flexDirection: 'row',
+     alignItems: 'center', 
+     justifyContent: 'center',
+  },
+  line: {
+      height: 2,
+      backgroundColor: '#F0F0F0',
+      flex: 1,
+      marginHorizontal: 5,
+  },
+  loadingImage: {
+        margin: 100,
+        height: width/3,
+        width: width/2,
+        resizeMode: Image.resizeMode.contain,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  BigImage: {
+    flex: 1,
+    height: Dimensions.get('window').height / 4,
+    resizeMode: Image.resizeMode.cover,
+  },
+  smallImage: {
+    flex: 0,
+    margin: 10,
+    height: Dimensions.get('window').width / 5,
+    width: Dimensions.get('window').width / 5,
+    //resizeMode: Image.resizeMode.cover,
+  },
+  listView: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  bigTextView: {
+    fontSize: 20,
+    marginTop: Dimensions.get('window').height / 4 - 30,
+    fontWeight: 'bold',
+    fontFamily: 'Cochin',
+    color: 'white',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  smallTextView: {
+    flex: 1,
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
